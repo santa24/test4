3551c7c68a6f3180b2c4b568b2edf73239b8b10f
santa24 <tonbog@gmail.com>
2019-09-30 13:40:59 +0900

    init web

---
+++ js/program.js
@@ -0,0 +1,16 @@
+let R_Square = document.getElementById("R-Square");
+let G_Square = document.getElementById("G-Square");
+let B_Square = document.getElementById("B-Square");
+
+
+R_Square.addEventListener('click',function(){
+    alert("R");
+},false);
+G_Square.addEventListener('click',function(){
+    alert("G");
+},false);
+B_Square.addEventListener('click',function(){
+    alert("B");
+},false);
+
+
